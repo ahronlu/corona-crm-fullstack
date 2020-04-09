206a86a736f87d510023fc7bfc87e01834501252
koftov <ahronlu@gmail.com>
2020-04-09 19:06:36 +0300

    deploy to heroku

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 8080;
 
 // Connect Database
 connectDB();
